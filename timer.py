e1ff0ddad936a4ccd7add4e46b35586528b74f6f
JVvix <fayedotly@gmail.com>
2020-07-09 13:02:39 -0700

    fixed bug

--- timer.py
+++ timer.py
@@ -1,53 +1,31 @@
-import time
-from playsound import playsound
+from tkinter import *
 
-def countdown(total_time):
-    total_time = int(total_time)
-    two_digit_seconds = 59
-    str_two_digit_seconds = str(two_digit_seconds)
-    str_minutes = str(minutes)
-    seconds = int(minutes * 60)
-    while(seconds != 0):
-        if int(str_two_digit_seconds) == 00:
-            if seconds > 59:
-                minutes -= 1
-                two_digit_seconds = 59
-                str_two_digit_seconds = "59"
-            seconds -= 1
-            two_digit_seconds -= 1
-            str_two_digit_seconds = str(two_digit_seconds)
-            print(str_minutes + ": " + two_digit_seconds)
-    playsound("beep-2.wav")
-    playsound("beep-2.wav")
-    playsound("beep-2.wav")
+root = Tk()
+root.geometry("300x300")
 
-input_time = input("how much time do you plan to time for? ")
-if input_time[-1] == "h":
-    seconds = int(time[:-1]) * 3600
-    #hours = time[:-1]
-if input_time[-1] == "m":
-    #minutes = time[:-1]
-    seconds = int(input_time[:-1]) * 60
-    
-print('hour: ' + input_time[:-1])
-print('seconds: ' + str(seconds))
+def change():
+    global timerlabel, timerinput, seconds
 
-while(seconds > 0):
-    #time.sleep(1)
-    #print(seconds)
-    display_minutes = seconds // 60
-    display_seconds = seconds % 60
-    if int(display_seconds) < 10:
-        display_seconds = "0" + str(display_seconds)
-    print(str(display_minutes) + ':' + str(display_seconds))
-    time.sleep(1)
-    seconds = seconds - 1
+    seconds -= 1
+    timerlabel.config(text=str(seconds))
+    timerlabel.grid(row=4, column=0)
+    if seconds != 0:
+        timerlabel.after(1000, change)
 
-playsound("beep-2.wav")
-playsound("beep-2.wav")
-playsound("beep-2.wav")
-#for i in range(len(time)):
+def start():
+    global timerlabel, timerinput, seconds
+    timerinput = entry.get()
+    if timerinput[-1] == "s":
+        seconds = int(timerinput[:-1])
+        timerlabel = Label(root, text=str(seconds))
+        timerlabel.grid(row=4, column=0)
+        if seconds != 0:
+            timerlabel.after(1000, change)
 
+entry = Entry(root, width=30)
+entry.grid(row=2, column=0, columnspan=20)
 
+button = Button(root, text="Start Timer", command=start)
+button.grid(row=3, column=0, columnspan=20)
 
-#countdown(time)
+root.mainloop()
