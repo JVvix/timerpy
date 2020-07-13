455ee3cebaddc3bf6e89eee8b10e5771ef1fb7d0
JVvix <fayedotly@gmail.com>
2020-07-12 22:14:33 -0700

    added pause and resume buttons

--- timer.py
+++ timer.py
@@ -2,28 +2,40 @@ from tkinter import *
 from winsound import *
 
 root = Tk()
-
-secondTimer = False
-minuteTimer = False
+timer_running = True
 
 def changeSeconds():
-    global seconds
+    global seconds, timer_running
     displayHours = seconds // 3600
     displayMinutes = seconds // 60
     displaySeconds = seconds % 60
     timerlabel = Label(root, text=str(displayHours) + ':' + str(displayMinutes) + ':' + str(displaySeconds))
-    timerlabel.grid(row=4, column=0)
-    if seconds >= 0:
-        seconds -= 1
-        timerlabel.after(1000, changeSeconds)
-    else:
-        for i in range(2):
-            PlaySound("beep-2.wav", SND_FILENAME)
-            timerlabel = Label(root, text=str(displayHours) + ':' + str(displayMinutes) + ':' + str(displaySeconds))
+    timerlabel.grid(row=7, column=0)
+    if timer_running:
+        if seconds >= 1:
+            seconds -= 1
+            timerlabel.after(1000, changeSeconds)
+        else:
+            for i in range(2):
+                PlaySound("beep-2.wav", SND_FILENAME)
+                timerlabel = Label(root, text='')
+                timer_running = True
+
+def pause():
+    global timer_running
+    timer_running = False
+
+def resume():
+    global seconds, timer_running, timerlabel
+    displayHours = seconds // 3600
+    displayMinutes = seconds // 60
+    displaySeconds = seconds % 60
+    timer_running = True
+    timerlabel = Label(root, text=str(displayHours) + ':' + str(displayMinutes) + ':' + str(displaySeconds))
+    timerlabel.after(1000, changeSeconds)
 
 def start():
-    global seconds
-    global timerlabel 
+    global seconds, timerlabel, timer_running
     timerlabel = Label(root)
     timerinput = entry.get()
     if timerinput[-1] == "s":
@@ -35,16 +47,21 @@ def start():
 
     timerlabel.after(1000, changeSeconds)
 
+def changeLabel():
+    global debug_label
+    debug_label = Label(root, text=str(timer_running))
+    debug_label.grid(row=6, column=0)
+
 entry = Entry(root, width=30)
 entry.grid(row=2, column=0, columnspan=20)
 
 button = Button(root, text="Start Timer", command=start)
 button.grid(row=3, column=0, columnspan=20)
 
+button2 = Button(root, text="Pause Timer", command=pause)
+button2.grid(row=4, column=0, columnspan=20)
+
+button3 = Button(root, text="Resume Timer", command=resume)
+button3.grid(row=5, column=0, columnspan=20)
+
 root.mainloop()
-
-
-#  90 
-#  90 % 60 = 30
-#  90 // 3600 
-
