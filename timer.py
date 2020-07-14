08ff57b2e2e1793ec8e17fea96d1afdd7563355b
JVvix <fayedotly@gmail.com>
2020-07-14 13:38:54 -0700

    switched timer_running to timer_pause and new button logic if timer is not or is paused

--- timer.py
+++ timer.py
@@ -2,16 +2,16 @@ from tkinter import *
 from winsound import *
 
 root = Tk()
-timer_running = True
+timer_paused = False
 
 def changeSeconds():
-    global seconds, timer_running
+    global seconds, timer_paused
     displayHours = seconds // (60 * 60)
     displayMinutes = seconds // 60
     displaySeconds = seconds % 60
     timerlabel = Label(root, text=str(displayHours) + ':' + str(displayMinutes) + ':' + str(displaySeconds))
     timerlabel.grid(row=7, column=0)
-    if timer_running:
+    if not timer_paused:
         if seconds >= 1:
             seconds -= 1
             timerlabel.after(1000, changeSeconds)
@@ -19,23 +19,30 @@ def changeSeconds():
             for i in range(3):
                 PlaySound("C:\\Users\\fly\\src\\timerpy\\beep-2.wav", SND_FILENAME)
             timerlabel = Label(root, text='')
-            timer_running = True
+            timer_paused = False
 
 def pause():
-    global timer_running
-    timer_running = False
+    global timer_paused
+    if timer_paused:
+        button2["state"] = DISABLED
+        button3["state"] = NORMAL
+
+    timer_paused = True
 
 def resume():
-    global seconds, timer_running, timerlabel
+    global seconds, timer_paused, timerlabel
+    if not timer_paused:
+        button2["state"] = NORMAL
+        button3["state"] = DISABLED
+
     displayHours = seconds // 3600
     displayMinutes = seconds // 60
     displaySeconds = seconds % 60
-    timer_running = True
-    timerlabel = Label(root, text=str(displayHours) + ':' + str(displayMinutes) + ':' + str(displaySeconds))
+    timer_paused = False
     timerlabel.after(1000, changeSeconds)
 
 def start():
-    global seconds, timerlabel, timer_running
+    global seconds, timerlabel, timer_paused
     timerlabel = Label(root)
     timerinput = entry.get()
     if timerinput[-1] == "s":
@@ -51,7 +58,7 @@ def start():
 
 def changeLabel():
     global debug_label
-    debug_label = Label(root, text=str(timer_running))
+    debug_label = Label(root, text=str(timer_paused))
     debug_label.grid(row=6, column=0)
 
 entry = Entry(root, width=30)
